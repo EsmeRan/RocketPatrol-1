0a4e54da0ed3de10e8918e70ff97cd1096c6c0af
EsmeRan <erangelh@ucsc.edu>
2022-04-18 22:02:51 -0700

    point breakdown, references, notes

--- src/main.js
+++ src/main.js
@@ -24,5 +24,27 @@ let keyF, keyR, keyLEFT, keyRIGHT;
 // global high score variable
 game.highScore = 0;
 
+//           *** HELP REFERENCES***
 // font help from https://www.webtips.dev/webtips/phaser/custom-fonts-in-phaser3
 // backgroundColor from https://stackoverflow.com/questions/59332460/how-to-set-background-color-of-phaser-3-game-during-runtime
+
+
+/*                  ***** POINT BREAKDOWN ******
+5 pts - track a displayed high score: the high score updates with
+        every run to show the highest score in game over screen
+
+5 pts - new smaller-faster-more valuable ship: the turtles go faster
+        than their frog counterparts in both easy and hard modes
+        and are worth 40 pts instead of 10
+
+10 pts - parallax scrolling: the background consists of 4 images that
+        scroll at different rates based on where they are - slower in the
+        back and faster in the front
+
+60 pts - redesign art, UI, and sound: all art assets have been changed or replaced;
+        the UI bars have been taken out, recolored, repositioned, and fonts have been changed;
+        menu, firing, and explosion sounds have been replaced by my own mouth noises
+*/
+
+// note: I attempted to change the font and it works, but I haven't been able to
+//       get rid of the warnings in the console
